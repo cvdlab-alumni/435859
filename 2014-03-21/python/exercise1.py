d9f9e085c0551064590fad02676e38ce50ce3826
Marco Virgadamo <virga91@gmail.com>
2014-03-22 12:44:21 +0100

    checkpoint 2

--- 2014-03-21/python/exercise1.py
+++ 2014-03-21/python/exercise1.py
@@ -66,6 +66,6 @@ floor2 = floor1
 
 floor3 = baseFloor
 
-building = STRUCT([floor0, T(3)(6)(floor1), T(3)(12)(floor2), T(3)(18)(floor3)])
-VIEW(building)
+two_and_half_model = STRUCT([floor0, T(3)(6)(floor1), T(3)(12)(floor2), T(3)(18)(floor3)])
+#VIEW(two_and_half_model)
 
